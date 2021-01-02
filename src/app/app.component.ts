03a0dfbffa214da31be36b012789525b466d0428
Oguzhan Filiz <oguzhan.filiz@ogr.dpu.edu.tr>
2021-01-02 14:46:41 +0300

    Display All

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from './model';
+import { Model,TodoItem } from './model';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +8,19 @@ import { Model } from './model';
 })
 export class AppComponent {
   model = new Model();
-
+  isDisplay = false;
   getName(){
     return this.model.user;
   }
   getitems(){
+    if(this.isDisplay){
+      return this.model.items;
+    }
     return this.model.items.filter(item=>!item.action);
+  }
+  addItem(value){
+    if(value!=""){
+      this.model.items.push(new TodoItem(value,false));
+    }
   }
 }
